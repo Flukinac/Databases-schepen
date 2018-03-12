dea8170ebc2f9438494ba1b4f711fbb139619cd6
Steven Visser <sevisser1@gmail.com>
2018-03-12 16:33:56 +0100

    nieuw

--- databasesschepen.php
+++ databasesschepen.php
@@ -1,8 +1,35 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$servername = "localhost";
+$username = "root";
+$password = "";      
+$db = "voorbeelddatabase";
 
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $db);
+echo "connected";
+
+?>
+<html>
+    <body>
+        <form action=<?php echo $_SERVER['PHP_SELF']; ?> method="post">    
+                <input type="text" placeholder="schepen" name="schepen">
+                <input type="text" placeholder="history" name="history">
+                <input type="text" placeholder="keurmerk" name="keurmerk">
+                <input type="submit" value="submit" name="submit">
+        </form>
+    </body>
+</html>
+        
+<?php
+
+
+$conn->query("INSERT INTO `schepen2`(`schepen`, `history`, `keurmerk`) VALUES ('".$_POST['schepen']."','".$_POST['history']."','".$_POST['keurmerk']."')");
+
+
+//$schepensql = "INSERT INTO `schepen2` (`schepen`) VALUE ('".$_POST['schepen']."')";
+//$conn->query($schepensql);
+//$historysql = "INSERT INTO `schepen2` (`history`) VALUE ('".$_POST['history']."')";
+//$conn->query($historysql);
+//$keurmerksql = "INSERT INTO `schepen2` (`keurmerk`) VALUE ('".$_POST['keurmerk']."')";
+//$conn->query($keurmerksql);
